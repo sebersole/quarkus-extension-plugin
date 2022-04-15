9a406f719f6a4eaca735fb41baf4c129b474e740
Steve Ebersole <steve@hibernate.org>
2022-04-15 06:41:02 -0500

    Improved tests

--- src/test/java/io/github/sebersole/quarkus/BasicPluginTests.java
+++ src/test/java/io/github/sebersole/quarkus/BasicPluginTests.java
@@ -4,12 +4,14 @@ import java.io.File;
 import java.nio.file.Path;
 
 import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.BuildTask;
 import org.gradle.testkit.runner.GradleRunner;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS;
 
 /**
  * @author Steve Ebersole
@@ -29,12 +31,47 @@ public class BasicPluginTests {
 
 		final BuildResult buildResult = gradleRunner.build();
 
+		final BuildTask compileDeployment = buildResult.task( ":compileDeploymentJava" );
+		assertThat( compileDeployment ).isNotNull();
+		assertThat( compileDeployment.getOutcome() ).isEqualTo( SUCCESS );
+
+		final BuildTask jarDeployment = buildResult.task( ":deploymentJar" );
+		assertThat( jarDeployment ).isNotNull();
+		assertThat( jarDeployment.getOutcome() ).isEqualTo( SUCCESS );
+
+
 		final File buildDir = new File( projectDir.toFile(), "build" );
 		assertThat( buildDir ).exists();
+
+		checkRuntime( buildDir );
+		checkDeployment( buildDir );
+		checkSpi( buildDir );
 	}
 
 	private void prepareProjectDir(Path projectDir) {
 		Copier.copyProject( "basic-extension/build.gradle", projectDir );
+	}
+
+	private void checkRuntime(File buildDir) {
+		assertThat( new File( buildDir, "classes/java/main" ) ).exists();
+		assertThat( new File( buildDir, "classes/java/test" ) ).exists();
+
+		final File jar = new File( buildDir, "libs/basic-extension-1.0-SNAPSHOT.jar" );
+		assertThat( jar ).exists();
+	}
+
+	private void checkDeployment(File buildDir) {
+		assertThat( new File( buildDir, "classes/java/deployment" ) ).exists();
+
+		final File jar = new File( buildDir, "libs/basic-extension-deployment-1.0-SNAPSHOT.jar" );
+		assertThat( jar ).exists();
+	}
+
+	private void checkSpi(File buildDir) {
+		assertThat( new File( buildDir, "classes/java/spi" ) ).exists();
+
+		final File jar = new File( buildDir, "libs/basic-extension-spi-1.0-SNAPSHOT.jar" );
+		assertThat( jar ).exists();
 	}
 
 }
